9d30731813d0f8dbbf91b69c080d8bf25ab162e3
AnkitKumar026 <99ankitkumar00@gmail.com>
2023-08-18 01:46:38 +0530

    Added
    add this file

---
+++ Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("START");
+        for(int i=1;i<=7;i++)
+        {
+            if(i==3)
+            {
+                System.out.println("KODNEST");
+                continue;
+            }
+            System.out.println("Technology");
+        }
+    }
+}
