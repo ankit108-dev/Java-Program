954e29ab5abac4bf0c1bbb7e7e432fee988b0e45
AnkitKumar026 <ankitkr108.live@gmail.com>
2023-08-18 02:53:47 +0530

    add this
    added

---
+++ program/src/program/Main.java
@@ -0,0 +1,15 @@
+package program;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int isbored=-1;
+		do
+		{
+			System.out.println("Study book");
+			isbored--;
+		}while(isbored>=0);
+
+	}
+
+}
